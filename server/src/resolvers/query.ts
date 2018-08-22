5ebbcafdec51fabd253814c0ef2d21268ba2cf4f
Filip Grzywok <rj23@protonmail.com>
2018-08-22 18:26:16 +0200

    Added channel query

--- server/src/resolvers/query.ts
+++ server/src/resolvers/query.ts
@@ -9,6 +9,22 @@ export default {
     return ctx.db.query.guild({ where: { id } }, info)
   },
 
+  async channel(parent, { id }, ctx: Context, info) {
+    const channel = await ctx.db.query.channel({ where: { id } }, {
+      ...info,
+      guildId: {
+        users: {
+          id,
+        },
+      },
+    })
+    const userId = getUserId(ctx)
+    if (!channel.guildId.users.some((el) => el.id === userId)) {
+      throw new Error('User not in guild')
+    }
+    return channel
+  },
+
   users(parent, args, ctx: Context, info) {
     return ctx.db.query.users({}, info)
   },
