c2c77b658ca1ea436677af36ef3f467f1b0d9f93
Bjornskjald <github@bjorn.ml>
2018-08-29 20:31:26 +0200

    Throw error if user is trying to send message but isn't in the guild

--- server/src/resolvers/mutation/message.ts
+++ server/src/resolvers/mutation/message.ts
@@ -3,6 +3,14 @@ import { getUserId, Context } from '../../utils'
 export default {
   async createMessage(parent, { channelId, content }, ctx: Context, info) {
     const userId = getUserId(ctx)
+    const guild = (await ctx.db.query.guilds({
+      where: {
+        channels_some: {
+          id: channelId,
+        },
+      },
+    }))[0]
+    if (!guild.users.find(user => user.id === userId)) throw new Error('User not in guild')
     const result = await ctx.db.mutation.createMessage(
       {
         data: {
