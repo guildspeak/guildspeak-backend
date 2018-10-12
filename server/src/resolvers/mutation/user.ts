961063a0b60d1b7bd5f98428078a0039fe3fc674
lunax <mlunaelumen@gmail.com>
2018-10-12 22:55:41 +0200

    feat: 🎸 unable disabled account to login
    disabled login function for disabled accounts

--- server/src/resolvers/mutation/user.ts
+++ server/src/resolvers/mutation/user.ts
@@ -1,7 +1,7 @@
 import { getUserId, Context } from '../../utils'
 
 export default {
-  async setdisableAccount(parent, { disabled }, ctx: Context, info) {
+  async disableAccount(parent, { disabled }, ctx: Context, info) {
     const userId = await getUserId(ctx)
     console.log(disabled)
     return ctx.db.mutation.updateUser({
